c8cacfdc4667193d4b5e17677b3ad17d7f7a8c72
Bogdan Kolbik <bogdan.kolbik@flixbus.com>
2019-08-21 14:48:52 +0200

    Adding opengraph data to atom feeds

--- cmd/atom.go
+++ cmd/atom.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/gorilla/feeds"
+	"github.com/otiai10/opengraph"
 	"gopkg.in/urfave/cli.v1"
 
 	"github.com/flix-tech/confs.tech.push/confs"
@@ -41,11 +42,24 @@ func generateAtomFeed(topic string, conferences []confs.Conference) (string, err
 
 	items := []*feeds.Item{}
 	for _, c := range conferences {
+		og, err := opengraph.Fetch(c.URL)
+		if err != nil {
+			og = opengraph.New(c.URL) // Ignoring the error, opengraph data is not critical
+		}
+
+		body := fmt.Sprintf("<p>%s・%s</p>", formatLocation(c), formatDateRange(c))
+		if og.Description != "" {
+			body += fmt.Sprintf("<p>%s</p>", og.Description)
+		}
+		if len(og.Image) > 0 {
+			body += fmt.Sprintf("<p><img src=\"%s\" alt=\"img\" /></p>", og.Image[0].URL)
+		}
+
 		items = append(items, &feeds.Item{
 			Title:       c.Name,
 			Link:        &feeds.Link{Href: c.URL},
 			Id:          c.URL,
-			Description: fmt.Sprintf("%s・%s", formatLocation(c), formatDateRange(c)),
+			Description: body,
 			Created:     now,
 		})
 	}
